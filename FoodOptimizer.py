5bcb097abaa01a02554908cf951fba3f85df66d8
Gergely Száz <gergely.szaz@liferay.com>
2018-11-17 02:05:23 +0100

    Scrape by XPATH

--- FoodOptimizer.py
+++ FoodOptimizer.py
@@ -10,3 +10,15 @@ config.read('config.INI')
 print('Scraping ' + config['DEFAULT']['url'])
 page = requests.get(config['DEFAULT']['url'])
 tree = html.fromstring(page.content)
+
+foods = tree.xpath(config['XPATH']['foods'])
+prices = tree.xpath(config['XPATH']['prices'])
+infos = tree.xpath(config['XPATH']['infos'])
+
+map(str.strip,foods)
+map(str.strip,prices)
+map(str.strip,infos)
+
+print(foods)
+print(prices)
+print(infos)
