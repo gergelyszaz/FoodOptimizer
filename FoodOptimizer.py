180ec925b95c63aad6d1213e21358a513a5798b6
Gergely Száz <gergely.szaz@liferay.com>
2018-11-17 22:44:43 +0100

    Map values to foods

--- FoodOptimizer.py
+++ FoodOptimizer.py
@@ -1,6 +1,7 @@
 from lxml import html
 import requests
 import configparser
+from pulp import *
 
 
 config = configparser.ConfigParser()
@@ -10,23 +11,10 @@ def clean(l):
     return list(filter(None,map(str.strip,l)))
 
 def getValues(s, l):
-    return list(
-        map(
-            lambda info: 
-                list(
-                    map(
-                        lambda v: v.replace(s,''),
-                        filter(
-                            lambda value: s in value,
-                            info
-                        )
-                    )
-                ),
-            l
-        )
-    )
+    # Because I hate you unconditionally
+    return [i.replace(s,'') for v in l for i in v if s in i]
 
-######
+###### Scrape data
 print('Scraping ' + config['DEFAULT']['url'])
 page = requests.get(config['DEFAULT']['url'])
 tree = html.fromstring(page.content)
@@ -39,17 +27,26 @@ foods=clean(foods)
 prices=clean(prices)
 infos=clean(infos)
 
+if( len(foods)!=len(infos) and len(foods)!=len(prices)):
+    print("The number of input datas do not match!")
+    exit(1)
+
 infos=list(map(lambda info: list(map(str.strip,info.split(','))),infos))
 kcals=getValues(' kcal', infos)
 fats=getValues('g zsír',infos)
 carbs=getValues('g szénh.',infos)
 prots=getValues('g fehérje',infos)
 
-print(len(foods))
-print(len(infos))
-print(len(prices))
-print(foods)
-print(prices)
-print (fats)
+kcals=[{foods[i]:kcals[i]} for i in range(0,len(foods)) ]
+carbs=[{foods[i]:carbs[i]} for i in range(0,len(foods)) ]
+prots=[{foods[i]:prots[i]} for i in range(0,len(foods)) ]
+fats=[{foods[i]:fats[i]} for i in range(0,len(foods)) ]
+
 print(kcals)
+print(fats)
+print(carbs)
 print(prots)
+
+
+
+
